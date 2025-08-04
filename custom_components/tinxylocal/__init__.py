51b8bc9589743fe9b202b7ae2e7c647795059a43
nishant713 <nishantkumar713@gmail.com>
2025-08-04 19:14:53 +0530

    Update __init__.py

--- custom_components/tinxylocal/__init__.py
+++ custom_components/tinxylocal/__init__.py
@@ -16,7 +16,7 @@ from .hub import TinxyLocalHub
 _LOGGER = logging.getLogger(__name__)
 
 # List the platforms that this integration will support.
-PLATFORMS: list[Platform] = [Platform.SWITCH, Platform.NUMBER]
+PLATFORMS: list[Platform] = [Platform.SWITCH, Platform.NUMBER, Platform.LOCK]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
